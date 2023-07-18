8846121f059f7822db6ea3db8acb176863a6b07a
Asguho <78410939+Asguho@users.noreply.github.com>
2023-07-18 14:11:13 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,63 @@
-export function add(a: number, b: number): number {
-  return a + b;
+async function getPackageIds() {
+  const command = new Deno.Command("winget", {
+    args: ["list"],
+  });
+  const output = await command.output();
+
+  const lines = new TextDecoder().decode(output.stdout).split("\n").slice(2);
+  const packages = lines.map((line) => {
+    return line.split(/(\s{2,}|…\s)/);
+  });
+  console.error(new TextDecoder().decode(output.stderr));
+  return packages.map((p) => p[2]);
 }
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+async function uninstallAllPackages() {
+  (await getPackageIds()).forEach(async (id) => {
+    const command = new Deno.Command("winget", {
+      args: ["uninstall", "--id", id, "--force", "--silent"],
+    });
+    const output = await command.output();
+    console.error(new TextDecoder().decode(output.stderr));
+    console.log(new TextDecoder().decode(output.stdout));
+  });
+}
+
+async function installPackage(id: string) {
+  await runCommand("winget", ["install", "-e", "--id", id, "--force", "--silent"]);
+}
+
+async function runCommand(command: string, args: string[]) {
+  const output = await new Deno.Command(command, {
+    args,
+  }).output();
+  console.error(new TextDecoder().decode(output.stderr));
+  console.log(new TextDecoder().decode(output.stdout));
+}
+
+async function main() {
+  //  await uninstallAllPackages();
+
+  await runCommand("wsl", ["--install"]);
+
+  [
+    "Microsoft.WindowsTerminal",
+    "Microsoft.VisualStudioCode",
+    "Microsoft.PowerToys",
+    "Discord.Discord",
+    "Spotify.Spotify",
+    "GitHub.GitHubDesktop",
+    "Git.Git",
+    "PrismLauncher.PrismLauncher",
+    "WinSCP.WinSCP",
+    "PuTTY.PuTTY",
+    "Docker.DockerDesktop",
+    "Google.Chrome",
+    "Mozilla.Thunderbird",
+    "Microsoft.PowerShell",
+    "Mozilla.Firefox",
+    "Microsoft.Office",
+  ].forEach(async (id) => {
+    await installPackage(id);
+  });
 }
